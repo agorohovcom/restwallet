515190009e52574674faa9dd7e3ddfbf1c16eda4
agorohov <aleksandr.gorohov@gmail.com>
2024-11-02 11:31:44 +0300

    initial commit

---
+++ src/main/java/com/agorohov/restwallet/RestWalletApplication.java
@@ -0,0 +1,13 @@
+package com.agorohov.restwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agorohov/restwallet/RestWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agorohov.restwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

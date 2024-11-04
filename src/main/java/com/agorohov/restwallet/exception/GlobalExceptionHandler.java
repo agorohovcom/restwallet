37a79d16461eb055718b2263b6b3ed6b9e6497d2
agorohov <aleksandr.gorohov@gmail.com>
2024-11-05 00:53:55 +0300

    Добавил volume для хранения логов

--- src/main/java/com/agorohov/restwallet/exception/GlobalExceptionHandler.java
+++ src/main/java/com/agorohov/restwallet/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.agorohov.restwallet.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -14,11 +16,15 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(WalletNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleWalletNotFoundException(WalletNotFoundException e) {
         Map<String, String> response = new HashMap<>();
         response.put("error", e.getMessage());
+
+        logger.error(e.getMessage());
+
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
@@ -26,6 +32,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleNotEnoughBalanceException(NotEnoughBalanceException e) {
         Map<String, String> response = new HashMap<>();
         response.put("error", e.getMessage());
+
+        logger.error(e.getMessage());
+
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
@@ -33,6 +42,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleTooLargeBalanceException(TooLargeBalanceException e) {
         Map<String, String> response = new HashMap<>();
         response.put("error", e.getMessage());
+
+        logger.error(e.getMessage());
+
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
@@ -42,16 +54,24 @@ public class GlobalExceptionHandler {
     })
     public ResponseEntity<Map<String, String>> handleInvalidArgumentExceptions(Exception e) {
         Map<String, String> response = new HashMap<>();
-        response.put("error", "Запрос не может быть прочитан, проверьте передаваемые аргументы");
+        String message = "Запрос не может быть прочитан, проверьте передаваемые аргументы";
+        response.put("error", message);
+
+        logger.error(message);
+
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         Map<String, String> response = new LinkedHashMap<>();
-        response.put("error", "Ошибка валидации");
+        String message = "Ошибка валидации";
+        response.put("error", message);
         e.getBindingResult().getFieldErrors().forEach(error ->
                 response.put(error.getField(), error.getDefaultMessage()));
+
+        logger.error("{}: {}", message, response);
+
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/agorohov/restwallet/service/WalletService.java
+++ src/main/java/com/agorohov/restwallet/service/WalletService.java
@@ -5,6 +5,8 @@ import com.agorohov.restwallet.exception.TooLargeBalanceException;
 import com.agorohov.restwallet.exception.WalletNotFoundException;
 import com.agorohov.restwallet.model.Wallet;
 import com.agorohov.restwallet.repository.WalletRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +15,8 @@ import java.util.UUID;
 
 @Service
 public class WalletService {
+    Logger logger = LoggerFactory.getLogger(WalletService.class);
+
     private final WalletRepository repository;
 
     public WalletService(WalletRepository repository) {
@@ -21,9 +25,11 @@ public class WalletService {
 
     @Transactional
     public Wallet deposit(UUID id, BigDecimal amount) {
+        logger.info("Метод deposit был вызван с параметрами {} и {}", id, amount);
+
         Wallet wallet = findById(id);
         BigDecimal newBalance = wallet.getBalance().add(amount);
-        BigDecimal limit = new BigDecimal("99999999999999999");
+        BigDecimal limit = new BigDecimal("9999999999999999");
         if (newBalance.compareTo(limit) > 0) {
             throw new TooLargeBalanceException("На балансе не может быть больше, чем 9999999999999999");
         }
@@ -33,6 +39,8 @@ public class WalletService {
 
     @Transactional
     public Wallet withdraw(UUID id, BigDecimal amount) {
+        logger.info("Метод withdraw был вызван с параметрами {} и {}", id, amount);
+
         Wallet wallet = findById(id);
         BigDecimal newBalance = wallet.getBalance().subtract(amount);
         if (newBalance.compareTo(BigDecimal.ZERO) < 0) {
@@ -43,6 +51,8 @@ public class WalletService {
     }
 
     public Wallet findById(UUID walletId) {
+        logger.info("Метод findById был вызван с параметром {}", walletId);
+
         return repository.findById(walletId)
                 .orElseThrow(() -> new WalletNotFoundException("Кошелёк не найден"));
     }
